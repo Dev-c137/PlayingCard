c7f8f3f477e1ccbfafae3268b9258b19e5a9280b
Devang <pawar.devang@gmail.com>
2018-02-19 17:19:07 +0530

    lecture 6 all demo done. some bug causing cards to show "?" in ranks looking into that

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var  deck = PlayingCardDeck()
+     
+    @IBOutlet weak var playingCardVIew: PlayingCardView!{
+        didSet{
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector (nextCard))
+            swipe.direction = [.left, .right]
+            playingCardVIew.addGestureRecognizer(swipe)
+            let pinch = UIPinchGestureRecognizer(target: playingCardVIew, action: #selector (playingCardVIew.adjustFaceCardScale(byHandlingGestureRecognizerBy:)))
+            playingCardVIew.addGestureRecognizer(pinch)
+        }
+    }
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state{
+        case .ended:
+            playingCardVIew.isFaceUp = !playingCardVIew.isFaceUp
+        default:
+            break
+        }
+    }
+    
+    
+    @objc func nextCard(){
+        if let card = deck.draw(){
+            playingCardVIew.rank = card.rank.order
+            playingCardVIew.suit = card.suit.rawValue
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
 
